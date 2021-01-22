69213b8669a09e3763f489cbf891286eb1920d04
anthonyhaffey <anthony.haffey@gmail.com>
2021-01-22 12:00:47 +0000

    data functionality tidy up

--- web/App/Studies/SheetEditor/SheetActions.js
+++ web/App/Studies/SheetEditor/SheetActions.js
@@ -476,14 +476,19 @@ $("#run_btn").on("click",function(){
         */
 			}
       if(typeof(github_json) == "undefined"){
-        organization = "You need to click the github button to see this";
-        repository = "";
-      } else if(github_json.organization !== ""){
-        var organization = github_json.organization;
+        try{
+          github_json = JSON.parse(Collector.electron.git.load_master());
+          var organization = github_json.organization;
+          var repository   = github_json.repository;
+        } catch(error){
+          organization = "Your github json seems broken";
+          repository = "";
+        }
       } else {
-        var organization = github_json.username;
+        var organization = github_json.organization;
         var repository   = github_json.repository;
       }
+
       var github_url =  "https://"                          +
                         organization                        +
                         ".github.io"                        + "/" +
