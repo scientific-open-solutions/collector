6aa060394e44b2f1a7ed5d949527a246c4f1d10e
anthonyhaffey <anthony.haffey@gmail.com>
2021-03-27 19:35:31 +0000

    added Nico as a contributor. Included "timer" functionality

--- web/App/Code/Collector.js
+++ web/App/Code/Collector.js
@@ -181,6 +181,36 @@ Collector = {
 			document.body.removeChild(elem);
 		}
 	},
+  //https://stackoverflow.com/a/20745721/4490801
+  timer: function(callback, delay) {
+    var id, started, remaining = delay, running;
+    this.start = function() {
+      running = true
+      started = new Date()
+      id = setTimeout(callback, remaining)
+    }
+
+    this.pause = function() {
+      running = false
+      clearTimeout(id)
+      remaining -= new Date() - started
+    }
+
+    this.getTimeLeft = function() {
+      if (running) {
+        this.pause()
+        this.start()
+      }
+      return remaining
+    }
+
+    this.getStateRunning = function() {
+      return running
+    }
+
+    this.start()
+  },
+
 	version: "cat"
 };
 
