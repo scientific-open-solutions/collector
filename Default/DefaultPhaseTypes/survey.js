0d8b310c8ebb52d83b7520bb721681e5c7b28a8c
CG Dobson <chrisguydobson@gmail.com>
2023-09-30 12:50:28 +0100

    Minor old tweaks and fixing assets folder issue

--- Default/DefaultPhaseTypes/survey.js
+++ Default/DefaultPhaseTypes/survey.js
@@ -1583,11 +1583,14 @@ function write_survey(this_survey, this_id) {
     response_check(this);
   });
 
-  $("#" + this_id).show(0); //scroll to top
-  $("#" + this_id).animate({
-    "height": "90%"
-  });
+  //scroll to top 
+  $("#" + this_id).show(0); 
 
+  //(hopeful) Firefox fix
+  $("#" + this_id).animate({
+    "height": window.innerHeight + "px",
+    "top": (parseFloat(window.innerHeight/2)) + "px"
+  });
 
   //show("slide", {direction: "down" }, "slow");
 
